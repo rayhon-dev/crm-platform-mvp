5b626478de7519382404831d4db91b517abe9007
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2026-07-01 11:57:16 +0500

    feat: add Course and Group models

--- apps/groups/models.py
+++ apps/groups/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from apps.courses.models import Course
+from apps.teachers.models import Teacher
 
-# Create your models here.
+
+class Group(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = 'active', 'Active'
+        FINISHED = 'finished', 'Finished'
+        PENDING = 'pending', 'Pending'
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='groups')
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, related_name='groups')
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+
+    def __str__(self):
+        return self.name
