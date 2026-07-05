930e23c3067b7a606f13144c4070acef9bf0cdff
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2026-07-05 15:51:04 +0500

    feat: add custom error handler with standard response format

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'EXCEPTION_HANDLER': 'apps.users.exceptions.custom_exception_handler',
 }
 
 SPECTACULAR_SETTINGS = {
